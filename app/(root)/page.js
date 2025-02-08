f71f02cc220fcef03834960181590b3884a70367
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-02-08 21:11:58 +0400

    File base routing created

---
+++ app/(root)/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="">
+      <h1 className="text-3xl bg-read-500 font-bold underline">
+        Welcome to my world
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import reactPlugin from "eslint-plugin-react";
-import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
-import importPlugin from "eslint-plugin-import";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import importPlugin from 'eslint-plugin-import';
 
 // Convert file URL to directory path
 const __filename = fileURLToPath(import.meta.url);
@@ -16,16 +15,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   // Apply Next.js core web vitals config
   ...compat.extends("next/core-web-vitals"),
-  eslintPluginPrettier,
+
   // Add your custom ESLint configuration
   {
-    files: ["**/*.js", "**/*.jsx"],
+    files: ['**/*.js', '**/*.jsx'],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
         browser: true,
       },
@@ -37,68 +36,66 @@ const eslintConfig = [
     },
     plugins: {
       react: reactPlugin,
-      "jsx-a11y": jsxA11yPlugin,
+      'jsx-a11y': jsxA11yPlugin,
       import: importPlugin,
     },
     rules: {
-      "no-console": "off",
-      "jsx-a11y/label-has-associated-control": "off",
-      "no-nested-ternary": "off",
-      "consistent-return": "off",
-      "no-alert": "off",
-      "react/jsx-no-constructed-context-values": "off",
-      "import/extensions": "off",
-      "react/prop-types": "off",
-      "linebreak-style": "off",
-      "react/state-in-constructor": "off",
-      "import/prefer-default-export": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-props-no-spreading": "off",
-      "jsx-a11y/no-noninteractive-element-interactions": "off",
-      "react/function-component-definition": [
-        "error",
+      'no-console': 'off',
+      'jsx-a11y/label-has-associated-control': 'off',
+      'no-nested-ternary': 'off',
+      'consistent-return': 'off',
+      'no-alert': 'off',
+      'react/jsx-no-constructed-context-values': 'off',
+      'import/extensions': 'off',
+      'react/prop-types': 'off',
+      'linebreak-style': 'off',
+      'react/state-in-constructor': 'off',
+      'import/prefer-default-export': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'jsx-a11y/no-noninteractive-element-interactions': 'off',
+      'react/function-component-definition': [
+        'error',
         {
-          namedComponents: "arrow-function",
-          unnamedComponents: "arrow-function",
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
         },
       ],
-      "max-len": ["error", 550],
-      "no-multiple-empty-lines": [
-        "error",
+      'max-len': ['error', 550],
+      'no-multiple-empty-lines': [
+        'error',
         {
           max: 1,
           maxEOF: 1,
         },
       ],
-      "no-underscore-dangle": [
-        "error",
+      'no-underscore-dangle': [
+        'error',
         {
-          allow: ["_d", "_dh", "_h", "_id", "_m", "_n", "_t", "_text"],
+          allow: ['_d', '_dh', '_h', '_id', '_m', '_n', '_t', '_text'],
         },
       ],
-      "object-curly-newline": "off",
-      "react/jsx-filename-extension": "off",
-      "react/jsx-one-expression-per-line": "off",
-      "jsx-a11y/click-events-have-key-events": "off",
-      "jsx-a11y/alt-text": "off",
-      "jsx-a11y/no-autofocus": "off",
-      "jsx-a11y/no-static-element-interactions": "off",
-      "react/no-array-index-key": "off",
-      "jsx-a11y/anchor-is-valid": [
-        "error",
+      'object-curly-newline': 'off',
+      'react/jsx-filename-extension': 'off',
+      'react/jsx-one-expression-per-line': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/alt-text': 'off',
+      'jsx-a11y/no-autofocus': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'react/no-array-index-key': 'off',
+      'jsx-a11y/anchor-is-valid': [
+        'error',
         {
-          components: ["Link"],
-          specialLink: ["to", "hrefLeft", "hrefRight"],
-          aspects: ["noHref", "invalidHref", "preferButton"],
+          components: ['Link'],
+          specialLink: ['to', 'hrefLeft', 'hrefRight'],
+          aspects: ['noHref', 'invalidHref', 'preferButton'],
         },
       ],
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
 ];
-
-export default eslintConfig;
