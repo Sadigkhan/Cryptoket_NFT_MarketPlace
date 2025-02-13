6cc13980cc0e44a8d50ad43ee4c6f1cb965287f5
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-02-13 17:41:36 +0400

    NFTContext Created Successfully

--- app/layout.js
+++ app/layout.js
@@ -3,22 +3,25 @@ import ThemeProvider from "@/context/Theme";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Script from "next/script";
+import { NFTProvider } from "@/context/NFTContext";
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen">
-        <ThemeProvider attribute="class" defaultTheme="system" >
-          <div className="min-h-screen bg-white dark:bg-nft-dark">
-            <Navbar />
-            <div>{children}</div>
-            <Footer />
-          </div>
-          <Script
-            src="https://kit.fontawesome.com/6f55973034.js"
-            crossOrigin="anonymous"
-          />
-        </ThemeProvider>
+        <NFTProvider>
+          <ThemeProvider attribute="class" defaultTheme="system">
+            <div className="min-h-screen bg-white dark:bg-nft-dark">
+              <Navbar />
+              <div>{children}</div>
+              <Footer />
+            </div>
+            <Script
+              src="https://kit.fontawesome.com/6f55973034.js"
+              crossOrigin="anonymous"
+            />
+          </ThemeProvider>
+        </NFTProvider>
       </body>
     </html>
   );
--- components/Input.jsx
+++ components/Input.jsx
@@ -1,6 +1,10 @@
 import React, { useContext } from 'react';
+import { NFTContext } from '@/context/NFTContext';
 
 const Input = ({ inputType, title, placeholder, handleClick }) => {
+
+  const {nftCurrency}=useContext(NFTContext);
+
   return (
     <div className="mt-10 w-full">
       <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">{title}</p>
@@ -13,7 +17,7 @@ const Input = ({ inputType, title, placeholder, handleClick }) => {
             placeholder={placeholder}
             onChange={handleClick}
           />
-          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">ETH</p>
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">{nftCurrency}</p>
         </div>
       ) : inputType === 'textarea' ? (
         <textarea
