55a263122a10c9980f33b3480037cb9bd7c49e24
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-02-16 23:56:41 +0400

    Re deployed because of fetching problem

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -72,6 +72,7 @@ export const NFTProvider = ({ children }) => {
       const tokenURI = await contract.tokenURI(tokenId);
       const {data:{image,name,description}}= await axios.get(tokenURI);
       const price = ethers.utils.formatUnits(unformattedPrice.toString(),"ether");
+      
 
       return {
         price,
@@ -84,6 +85,7 @@ export const NFTProvider = ({ children }) => {
         tokenURI
       }
     }))
+    console.log(items)
     return items;
   };
 
